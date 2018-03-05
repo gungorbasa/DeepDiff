758cb2ad395e9fc13ec8fd5e1ae509642739b360
Güngör <gungorbasa@gmail.com>
2018-03-06 00:13:54 +0300

    Texture example is added as a seperate project.

--- Example/DeepDiffDemo/DeepDiffDemo/Sources/AppDelegate.swift
+++ Example/DeepDiffDemo/DeepDiffDemo/Sources/AppDelegate.swift
@@ -16,14 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let collectionController = CollectionViewController()
     collectionController.tabBarItem.image = UIImage(named: "collection")
 
-    let textureController = TextureTableController()
-    textureController.tabBarItem.image = UIImage(named: "table")
-
     let tabController = UITabBarController()
 
     tabController.viewControllers = [
       UINavigationController(rootViewController: tableController),
-      UINavigationController(rootViewController: textureController),
       UINavigationController(rootViewController: collectionController)
     ]
 
